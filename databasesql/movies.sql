c0ab24dcee997307caad03c8743ed46f1596ab92
Emilio Vargas <desarrollador@tumembresiawow.com>
2022-04-29 00:59:57 -0500

    cambiando script de la base de datos

--- databasesql/movies.sql
+++ databasesql/movies.sql
@@ -1,82 +1,75 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.0-rc1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: movies
--- ------------------------------------------------------
--- Server version	8.0.28-0ubuntu0.20.04.3
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 29-04-2022 a las 07:49:58
+-- Versión del servidor: 8.0.29
+-- Versión de PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `categories`
+-- Base de datos: `moviesemjvp`
 --
 
-DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categories` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `categories`
+-- Estructura de tabla para la tabla `movies`
 --
 
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `movies`
---
-
-DROP TABLE IF EXISTS `movies`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `movies` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `cover_image` text COLLATE utf8mb4_general_ci NOT NULL,
-  `description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `id` int NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cover_image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `duration` int DEFAULT NULL,
   `categories` json DEFAULT NULL,
   `youtube_trailer_url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `seen` tinyint(1) DEFAULT '0',
   `release_date` date NOT NULL,
-  `rate` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `rate` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `movies`
+-- Volcado de datos para la tabla `movies`
 --
 
-LOCK TABLES `movies` WRITE;
-/*!40000 ALTER TABLE `movies` DISABLE KEYS */;
-INSERT INTO `movies` VALUES (1,'DOCTOR STRANGE IN THE MULTIVERSE OF MADNESS','https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster.png/0/images/masterrepository/Fandango/222999/FND_poster_DrStrange_PreSale[94].jpg','In Marvel Studios’ “Doctor Strange in the Multiverse of Madness,” the MCU unlocks the Multiverse and pushes its boundaries further than ever before. Journey into the unknown with Doctor Strange, who, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.',192,'{\"name\": \"fantasy\"}','https://www.youtube.com/watch?v=aWzlQ2N6qqg',0,'2022-05-06',0),(2,'DOCTOR STRANGE IN THE MULTIVERSE OF MADNESS','https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster.png/0/images/masterrepository/Fandango/222999/FND_poster_DrStrange_PreSale[94].jpg','In Marvel Studios’ “Doctor Strange in the Multiverse of Madness,” the MCU unlocks the Multiverse and pushes its boundaries further than ever before. Journey into the unknown with Doctor Strange, who, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.',192,'[{\"name\": \"fantasy\"}, {\"name\": \"sc-fi\"}]','https://www.youtube.com/watch?v=aWzlQ2N6qqg',0,'2022-05-06',0),(3,'DOCTOR STRANGE IN THE MULTIVERSE OF MADNESS','https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster.png/0/images/masterrepository/Fandango/222999/FND_poster_DrStrange_PreSale[94].jpg','In Marvel Studios’ “Doctor Strange in the Multiverse of Madness,” the MCU unlocks the Multiverse and pushes its boundaries further than ever before. Journey into the unknown with Doctor Strange, who, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.',192,'[{\"name\": \"fantasy\"}, {\"name\": \"sc-fi\"}]','https://www.youtube.com/watch?v=aWzlQ2N6qqg',0,'2022-04-11',0);
-/*!40000 ALTER TABLE `movies` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `movies` (`id`, `title`, `cover_image`, `description`, `duration`, `categories`, `youtube_trailer_url`, `seen`, `release_date`, `rate`) VALUES
+(1, 'DOCTOR STRANGE IN THE MULTIVERSE OF MADNESS', 'https://images.fandango.com/ImageRenderer/200/0/redesign/static/img/default_poster.png/0/images/masterrepository/Fandango/222999/FND_poster_DrStrange_PreSale[94].jpg', 'In Marvel Studios’ “Doctor Strange in the Multiverse of Madness,” the MCU unlocks the Multiverse and pushes its boundaries further than ever before. Journey into the unknown with Doctor Strange, who, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.', 192, '{\"name\": \"fantasy\"}', 'https://www.youtube.com/watch?v=aWzlQ2N6qqg', 0, '2022-05-06', 0),
+(2, 'Fantastic Beasts: The Secrets of Dumbledore', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR05Pz4KhR1duYx2RsHeZBYkNlcA9_UDv50BCUcjVguu4loZZA0', 'In Marvel Studios’ “Doctor Strange in the Multiverse of Madness,” the MCU unlocks the Multiverse and pushes its boundaries further than ever before. Journey into the unknown with Doctor Strange, who, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.', 192, '[{\"name\": \"fantasy\"}]', 'https://www.youtube.com/watch?v=Y9dr2zw-TXQ', 0, '2022-04-14', 0),
+(3, 'Sonic the Hedgehog 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9lJeR2hUpyF71LLRDXWgfpj8sdowJVyLbMb1z4UZTjs68eRyQ', 'After settling in Green Hills, Sonic is eager to prove that he has what it takes to be a true hero. His test comes when Dr. Robotnik returns with a new partner, Knuckles, in search of a mystical emerald that has the power to destroy civilizations. Sonic teams up with his own sidekick, Tails, and together they embark on a globe-trotting journey to find the emerald before it falls into the wrong hands.', 192, '[{\"name\": \"fantasy\"}, {\"name\": \"sc-fi\"}]', 'https://whttps://www.youtube.com/watch?v=47r8FXYZWNUww.youtube.com/watch?v=aWzlQ2N6qqg', 0, '2022-04-07', 0);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-04-28 21:19:02
